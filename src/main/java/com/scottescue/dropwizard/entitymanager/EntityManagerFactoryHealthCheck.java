3e8d96d0c51d71551b4bb4454568156f9b64b47a
Scott Escue <scott.escue@southernfiredsoftware.com>
2015-12-17 01:26:42 -0600

    Adding EntityManagerFactory health check.

---
+++ src/main/java/com/scottescue/dropwizard/entitymanager/EntityManagerFactoryHealthCheck.java
@@ -0,0 +1,67 @@
+package com.scottescue.dropwizard.entitymanager;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.dropwizard.db.TimeBoundHealthCheck;
+import io.dropwizard.util.Duration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+
+public class EntityManagerFactoryHealthCheck extends HealthCheck {
+    private final EntityManagerFactory entityManagerFactory;
+    private final String validationQuery;
+    private final TimeBoundHealthCheck timeBoundHealthCheck;
+
+    public EntityManagerFactoryHealthCheck(EntityManagerFactory entityManagerFactory,
+                                           String validationQuery) {
+        this(MoreExecutors.newDirectExecutorService(), Duration.seconds(0), entityManagerFactory, validationQuery);
+    }
+
+    public EntityManagerFactoryHealthCheck(ExecutorService executorService,
+                                           Duration duration,
+                                           EntityManagerFactory entityManagerFactory,
+                                           String validationQuery) {
+        this.entityManagerFactory = entityManagerFactory;
+        this.validationQuery = validationQuery;
+        this.timeBoundHealthCheck = new TimeBoundHealthCheck(executorService, duration);
+    }
+
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    @Override
+    protected HealthCheck.Result check() throws Exception {
+        return timeBoundHealthCheck.check(new Callable<Result>() {
+            @Override
+            public Result call() throws Exception {
+                final EntityManager entityManager = entityManagerFactory.createEntityManager();
+                try {
+                    final EntityTransaction entityTransaction = entityManager.getTransaction();
+                    entityTransaction.begin();
+                    try {
+                        entityManager.createNativeQuery(validationQuery).getResultList();
+                        entityTransaction.commit();
+                    } catch (Exception e) {
+                        if (entityTransaction.isActive()) {
+                            entityTransaction.rollback();
+                        }
+                        throw e;
+                    }
+                } finally {
+                    entityManager.close();
+                }
+                return Result.healthy();
+            }
+        });
+    }
+}
---
+++ src/test/java/com/scottescue/dropwizard/entitymanager/EntityManagerFactoryHealthCheckTest.java
@@ -0,0 +1,80 @@
+package com.scottescue.dropwizard.entitymanager;
+
+import com.codahale.metrics.health.HealthCheck;
+import org.junit.Test;
+import org.mockito.InOrder;
+
+import javax.persistence.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+
+public class EntityManagerFactoryHealthCheckTest {
+    private final EntityManagerFactory factory = mock(EntityManagerFactory.class);
+    private final EntityManagerFactoryHealthCheck healthCheck = new EntityManagerFactoryHealthCheck(factory,
+            "SELECT 1");
+
+    @Test
+    public void hasAnEntityManagerFactory() throws Exception {
+        assertThat(healthCheck.getEntityManagerFactory())
+                .isEqualTo(factory);
+    }
+
+    @Test
+    public void hasAValidationQuery() throws Exception {
+        assertThat(healthCheck.getValidationQuery())
+                .isEqualTo("SELECT 1");
+    }
+
+    @Test
+    public void isHealthyIfNoExceptionIsThrown() throws Exception {
+        final EntityManager entityManager = mock(EntityManager.class);
+        when(factory.createEntityManager()).thenReturn(entityManager);
+
+        final EntityTransaction transaction = mock(EntityTransaction.class);
+        when(entityManager.getTransaction()).thenReturn(transaction);
+
+        final Query query = mock(Query.class);
+        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
+
+        assertThat(healthCheck.execute())
+                .isEqualTo(HealthCheck.Result.healthy());
+
+        final InOrder inOrder = inOrder(factory, entityManager, transaction, query);
+        inOrder.verify(factory).createEntityManager();
+        inOrder.verify(entityManager).getTransaction();
+        inOrder.verify(entityManager).createNativeQuery("SELECT 1");
+        inOrder.verify(query).getResultList();
+        inOrder.verify(transaction).commit();
+        inOrder.verify(entityManager).close();
+    }
+
+    @Test
+    public void isUnhealthyIfAnExceptionIsThrown() throws Exception {
+        final EntityManager entityManager = mock(EntityManager.class);
+        when(factory.createEntityManager()).thenReturn(entityManager);
+
+        final EntityTransaction transaction = mock(EntityTransaction.class);
+        when(entityManager.getTransaction()).thenReturn(transaction);
+        when(transaction.isActive()).thenReturn(true);
+
+        final Query query = mock(Query.class);
+        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
+        when(query.getResultList()).thenThrow(new PersistenceException("OH NOE"));
+
+        assertThat(healthCheck.execute().isHealthy())
+                .isFalse();
+
+        final InOrder inOrder = inOrder(factory, entityManager, transaction, query);
+        inOrder.verify(factory).createEntityManager();
+        inOrder.verify(entityManager).getTransaction();
+        inOrder.verify(entityManager).createNativeQuery("SELECT 1");
+        inOrder.verify(query).getResultList();
+        inOrder.verify(transaction).rollback();
+        inOrder.verify(entityManager).close();
+
+        verify(transaction, never()).commit();
+    }
+}
