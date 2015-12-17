c0352377fdbbe61f8fb071d7b4f31a563cfcf8a1
Scott Escue <scott.escue@southernfiredsoftware.com>
2015-12-17 00:28:52 -0600

    Configuring Maven to deploy artifacts to new GitHub branch.  Updating Travis' after success script to trigger the deploy goal.  I also had to configure Travis, through the web UI, to ignore branches without a .travis.yml file.  Otherwise, deploying to the Github branch caused a new, failing Travis build.

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -6,3 +6,20 @@ if [[ "${TRAVIS_JDK_VERSION}" != "oraclejdk8" ]]; then
 fi
 
 mvn -B cobertura:cobertura coveralls:report
+
+if [[ -n ${TRAVIS_TAG} ]]; then
+    echo "Skipping deployment for tag \"${TRAVIS_TAG}\""
+    exit
+fi
+
+if [[ ${TRAVIS_BRANCH} != 'master' ]]; then
+    echo "Skipping deployment for branch \"${TRAVIS_BRANCH}\""
+    exit
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+    echo "Skipping deployment for pull request"
+    exit
+fi
+
+mvn -B deploy --settings maven_deploy_settings.xml -Dmaven.test.skip=true -Dsource.skip=true
