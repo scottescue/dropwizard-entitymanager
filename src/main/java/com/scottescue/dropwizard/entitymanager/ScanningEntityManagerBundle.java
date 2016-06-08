f6b98a6e2321c2967d520ecb47338b25c4812b1c
Scott Escue <scott.escue@southernfiredsoftware.com>
2016-06-08 13:11:53 -0500

    Reverting dropwizard-hibernate dependency.  Rather, the UnitOfWork annotation and classpath scanning for the EntityManager bundle are being included directly into the project.  Having the dropwizard API for Hibernate Session interaction on the classpath along with this EntityManager focused API is potentially confusing.

--- src/main/java/com/scottescue/dropwizard/entitymanager/ScanningEntityManagerBundle.java
+++ src/main/java/com/scottescue/dropwizard/entitymanager/ScanningEntityManagerBundle.java
@@ -1,7 +1,13 @@
 package com.scottescue.dropwizard.entitymanager;
 
+import com.google.common.collect.ImmutableList;
 import io.dropwizard.Configuration;
-import io.dropwizard.hibernate.ScanningHibernateBundle;
+import org.glassfish.jersey.server.internal.scanning.AnnotationAcceptingListener;
+import org.glassfish.jersey.server.internal.scanning.PackageNamesScanner;
+
+import javax.persistence.Entity;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Extension of EntityManagerBundle that scans given package for entities instead of giving them by hand.
@@ -16,6 +22,36 @@ public abstract class ScanningEntityManagerBundle<T extends Configuration> exten
     }
 
     protected ScanningEntityManagerBundle(String pckg, EntityManagerFactoryFactory entityManagerFactoryFactory) {
-        super(ScanningHibernateBundle.findEntityClassesFromDirectory(pckg), entityManagerFactoryFactory);
+        super(findEntityClassesFromDirectory(pckg), entityManagerFactoryFactory);
+    }
+
+    /**
+     * Method scanning given directory for classes containing JPA @Entity annotation
+     *
+     * @param pckg string with package containing JPA entities (classes annotated with @Entity annotation)
+     * @return ImmutableList with classes from given directory annotated with JPA @Entity annotation
+     */
+    public static ImmutableList<Class<?>> findEntityClassesFromDirectory(String pckg) {
+        @SuppressWarnings("unchecked")
+        final AnnotationAcceptingListener asl = new AnnotationAcceptingListener(Entity.class);
+        final PackageNamesScanner scanner = new PackageNamesScanner(new String[]{pckg}, true);
+
+        while (scanner.hasNext()) {
+            final String next = scanner.next();
+            if (asl.accept(next)) {
+                try (final InputStream in = scanner.open()) {
+                    asl.process(next, in);
+                } catch (IOException e) {
+                    throw new RuntimeException("AnnotationAcceptingListener failed to process scanned resource: " + next);
+                }
+            }
+        }
+
+        final ImmutableList.Builder<Class<?>> builder = ImmutableList.builder();
+        for (Class<?> clazz : asl.getAnnotatedClasses()) {
+            builder.add(clazz);
+        }
+
+        return builder.build();
     }
 }
