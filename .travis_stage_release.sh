a12f0c9ff38d62368418b517b2edaec7cbf7a576
Scott Escue <scott.escue@arrayinfotech.com>
2016-08-26 15:50:00 -0500

    Rebuilding the Travis-CI deployment scripts to eliminate all Ruby code.  The Stage Release script now sources the target/project.properties file to pull in values of interest from the Maven POM file.  I'm also updating the deploy script to allow Snapshot builds to be published to Sonatype.

--- .travis_stage_release.sh
+++ .travis_stage_release.sh
@@ -1,20 +1,80 @@
 #!/usr/bin/env bash
 
+function abort {
+    printf "Skipping deployment: ${1}\n" && exit 1
+}
+
+function is_valid_tag {
+    if [[ ! $1 =~ ^v[0-9]\.[0-9]\.[0-9]\-[0-9]$ ]]; then
+        return 1
+    fi
+}
+
+
+# Source the POM properties file written during the build
+source target/project.properties >/dev/null 2>&1
+
 echo
-echo "After-Succes Environment:"
+echo "Deployment Environment:"
 echo "  JDK Version: ${TRAVIS_JDK_VERSION}"
 echo "  Travis Tag: ${TRAVIS_TAG}"
 echo "  Travis Branch: ${TRAVIS_BRANCH}"
 echo "  POM Version: ${POM_VERSION}"
+echo "  POM URL: ${POM_URL}"
 echo "  POM SCM Tag: ${POM_SCM_TAG}"
+echo "  Dropwizard Version: ${DROPWIZARD_VERSION}"
 echo
 
 
-# .travis_assert_ready_to_deploy.rb performs all validation to determine if build should be deployed
-`ruby .travis_assert_ready_to_deploy.rb`
-
-# An exit status of 0 indicates all validations succeeded
-if [[ "${?}" == "0" ]]; then
-    printf "\nDeploying tagged version ${POM_VERSION}\n\n"
-    bash $DEPLOY_DIR/publish.sh
+# Ensure a project version is specified in the POM
+if [[ ! -n $POM_VERSION ]]; then
+    abort "Artifact version is not specified in the POM"
 fi
+
+# Ensure a SCM tag is specified in the POM
+if [[ ! -n $POM_SCM_TAG ]]; then
+    abort "SCM tag is not specified in the POM"
+fi
+
+if [[ -n $TRAVIS_TAG ]]; then
+    # This is a tag build
+
+    # Ensure we're building a valid release tag
+    if ! is_valid_tag $TRAVIS_TAG; then
+        abort "Tag '${TRAVIS_TAG}' is not a properly formatted release tag"
+    fi
+
+    # Ensure the project has a valid release version configured in the POM
+    if ! is_valid_tag "v${POM_VERSION}"; then
+        abort "This is a tag build, but ${POM_VERSION} is not a valid release version"
+    fi
+
+    # Ensure the Git tag, project version, project URL, SCM tag, and Dropwizard version are all in agreement
+    if [[ ! $POM_SCM_TAG == $TRAVIS_TAG ]]; then
+        abort "SCM tag '${POM_SCM_TAG}' in the POM does not match the '${TRAVIS_TAG}' tag being built"
+    fi
+    if [[ ! $POM_SCM_TAG == "v$POM_VERSION" ]]; then
+        abort "SCM tag '${POM_SCM_TAG}' in the POM does not match version '${POM_VERSION}' in the POM"
+    fi
+    if [[ ! "${POM_URL}" == "http://scottescue.com/dropwizard-entitymanager/${POM_VERSION}" ]]; then
+        abort "Project URL (${POM_URL}) in the POM should be http://scottescue.com/dropwizard-entitymanager/${POM_VERSION}"
+    fi
+    if [[ ! $POM_VERSION == ${DROPWIZARD_VERSION}\-* ]]; then
+        abort "Dropwizard version ${DROPWIZARD_VERSION} is not correct for project version ${POM_VERSION}"
+    fi
+
+    DEPLOY_TYPE="tagged release"
+else
+
+    # This is not a tag build, ensure the POM has a snapshot version
+    if [[ ! $POM_VERSION == *-SNAPSHOT ]]; then
+        abort "This is a snapshot build, but POM version '${POM_VERSION}' is not a snapshot"
+    fi
+
+    DEPLOY_TYPE="snapshot"
+fi
+
+
+# All validations have succeeded at this point. Let's publish!
+printf "Deploying ${DEPLOY_TYPE} version ${POM_VERSION}\n\n"
+bash $DEPLOY_DIR/publish.sh
